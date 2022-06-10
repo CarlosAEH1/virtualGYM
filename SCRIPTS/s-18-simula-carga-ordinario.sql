e2e7b0534be0631d45986e00f28c8c96b470139f
Erick Rocha <54082836+zartqrom@users.noreply.github.com>
2022-06-09 20:19:57 -0500

    Cargador diario

---
+++ SCRIPTS/s-18-simula-carga-ordinario.sql
@@ -0,0 +1,28 @@
+--@Integrantes:
+--              -Elizarraras Herrera Carlos Amador
+--              -Rocha García Erick Hazel
+--@Fecha:       05/06/2022
+--@Descripcion: Simula carga diaria por modulo.
+
+Prompt Conectando como usuario gimnasio01
+connect gimnasio01/gimnasio
+
+Prompt Cargando datos en módulo gimnasio.
+@s-18-dml-status-dispositivo/s-dml-10.sql
+commit;
+
+Prompt Conectando como usuario cliente01
+connect cliente01/cliente
+
+Prompt Cargando datos en módulo cliente.
+@s-18-dml-cliente/s-dml-10.sql
+@s-18-dml-sesion/s-dml-10.sql
+@s-18-dml-bitacora/s-dml-10.sql
+@s-18-dml-credencial/s-dml-10.sql
+@s-18-dml-reporte/s-dml-10.sql
+@s-18-dml-sensor/s-dml-10.sql
+@s-18-dml-sesion-dispositivo/s-dml-10.sql
+commit;
+
+Prompt Listo
+exit
